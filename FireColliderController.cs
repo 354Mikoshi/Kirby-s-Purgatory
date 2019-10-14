60b16195dc35edf42d21835b3c729491cb54222d
quietMikoshi <48008411+quietMikoshi@users.noreply.github.com>
2019-10-14 23:31:51 +0900

    Create FireColliderController.cs

---
+++ FireColliderController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FireColliderController : MonoBehaviour
+{
+    private void Start() {
+        Destroy(gameObject, 5f);
+    }
+
+    private void Update() {   
+    }
+}
